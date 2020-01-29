c762c07f4374d8a3ea313deee01e2d37ee2627a1
Mattias Åslund <mattias@trustfall.se>
2020-01-29 17:43:50 +0100

    Fixed styling of admin bar

--- src/templates/np2020/layout.tsx
+++ src/templates/np2020/layout.tsx
@@ -20,6 +20,31 @@ async function MyLayout(component: Promise<JSX.Element>, pipeline: RequestPipeli
                     <link rel="stylesheet" id="wp-block-library-css" href={pipeline.publicPath("css/dist/block-library/style.min.css")} type="text/css" media="all" />
                     <link rel="stylesheet" id="open-sans-css" href="https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;display=fallback&amp;ver=5.4-alpha-47108" type="text/css" media="all" />
                     <link rel='stylesheet' id='wporg-themes-css' href={pipeline.templatePath("css/style.css")} type='text/css' media='all' />
+
+                    <style dangerouslySetInnerHTML={{__html: `
+                        @media screen and (max-width: 782px) {
+                            #wpadminbar li#wp-admin-bar-log-in,
+                            #wpadminbar li#wp-admin-bar-register {
+                                display: block;
+                            }
+
+                            #wpadminbar li#wp-admin-bar-log-in a,
+                            #wpadminbar li#wp-admin-bar-register a {
+                                padding: 0 8px;
+                            }
+                        }
+                    `}} />
+                    <style type="text/css" media="print" dangerouslySetInnerHTML={{__html: `
+                        #wpadminbar { display:none; }
+                    `}} />
+                    <style type="text/css" media="screen" dangerouslySetInnerHTML={{__html: `
+                        html { margin-top: 32px !important; }
+                        * html body { margin-top: 32px !important; }
+                        @media screen and ( max-width: 782px ) {
+                            html { margin-top: 46px !important; }
+                            * html body { margin-top: 46px !important; }
+                        }
+                    `}} />
                 </head>
 
                 <body id="wordpress-org" className="repopackage-template-default single single-repopackage postid-25021 admin-bar no-customize-support modal-open">
